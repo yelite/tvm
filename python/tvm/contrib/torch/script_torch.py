521d7f68d205bd348ba55219ee49f25d924726ef
juda <yzhou@octoml.ai>
2022-04-21 20:25:22 -0700

    compile error

--- python/tvm/contrib/torch/script_torch.py
+++ python/tvm/contrib/torch/script_torch.py
@@ -1,35 +1,38 @@
 # pylint: disable=missing-module-docstring
 import torch
 import tvm
-from typing import List
+from typing import List, Union
 import torch.utils.dlpack
 
 class TVMScriptModule(torch.nn.Module):
-    def __init__(self, module : tvm.runtime.Module):
+    def __init__(self, module : Union[tvm.ir.module.IRModule, tvm.tir.function.PrimFunc]):
         super().__init__()
-        self.engine = None
-        
-        # TODO : switch to C++ for milestone 1.2
-        # self.engine = torch.classes.tvm_dsoop.TVMScriptModule(module, input_shape, output_shape, self.device, target)
         self.runtime_mod = tvm.build(module)
-        # params = [module.buffer_map[x] for x in module.params]
-        # self.inputs_shape = params[:-1]
-        # self.output_shape = params[-1]
+
 
     def forward(self, torch_inputs : List[torch.Tensor]) -> torch.Tensor :
-        # TODO : switch to C++ for milestone 1.2
-        # r"""Call tvm module to forward"""
-        # return self.engine.forward(torch_input)
-
         tensor_inputs = [tvm.nd.from_dlpack(torch.utils.dlpack.to_dlpack(i)) for i in torch_inputs]
-
-        # TODO : check the input shape
-        # assert tensor_numpy.shape == self.input_shape   
 
         self.runtime_mod(*tensor_inputs)
         torch_output = tensor_inputs[-1]
         torch_output = torch.utils.dlpack.from_dlpack(torch_output.to_dlpack())
         return torch_output
+
+class TVMScriptModuleWithCxx(torch.nn.Module):
+    def __init__(self, module : Union[tvm.ir.module.IRModule, tvm.tir.function.PrimFunc]):
+        super().__init__()
+        # we assume only 1 output for now
+        self.engine = torch.classes.tvm_dsoop.TVMScriptModule(len(module.params) - 1, 1, "cpu")
+        self.engine.load_IR_module(module)
+        
+
+    def forward(self, torch_inputs : List[torch.Tensor]) -> torch.Tensor :
+        # tensor_inputs = [tvm.nd.from_dlpack(torch.utils.dlpack.to_dlpack(i)) for i in torch_inputs]
+        # self.runtime_mod(*tensor_inputs)
+        # torch_output = tensor_inputs[-1]
+        # torch_output = torch.utils.dlpack.from_dlpack(torch_output.to_dlpack())
+        # return torch_output
+        pass
 
 
 def as_torch( func: tvm.ir.module.IRModule):
