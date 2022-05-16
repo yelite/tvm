60d2c4e2e6a01e196b1c334a4e4d0f989adc575d
juda <yzhou@octoml.ai>
2022-05-15 18:07:54 -0700

    nn.module

--- python/tvm/contrib/torch/pytorch_tuning.py
+++ python/tvm/contrib/torch/pytorch_tuning.py
@@ -8,7 +8,7 @@ import torch.utils.dlpack
 from tvm.meta_schedule import TuneConfig, tune_tir
 from tvm.meta_schedule.tune import tune_extracted_tasks, tune_relay
 import tempfile
-from typing import Union, Dict
+from typing import Union, Dict, List
 from tvm.script import tir as T
 
 @functools.lru_cache(None)
@@ -25,7 +25,26 @@ def tuning_relay(mod : tvm.ir.module.IRModule, params : Dict, config : TuneConfi
             work_dir=work_dir,
         )
         return rt_mod1
-    
+
+class RelayRTModule(torch.nn.Module):
+        def __init__(self, rt_mod : graph_executor.GraphModule):
+            super().__init__()
+            self.rt_mod = rt_mod
+
+        @staticmethod
+        def _to_torch_tensor(nd_tensor : tvm.nd.NDArray) -> torch.Tensor :
+            return torch.utils.dlpack.from_dlpack(nd_tensor.to_dlpack())
+
+        def forward(self, *torch_inputs : List[torch.Tensor]) -> torch.Tensor :
+            for idx, arg in enumerate(torch_inputs, 0):
+                if arg.dim() != 0:
+                    self.rt_mod.set_input(
+                        f"inp_{idx}",
+                        tvm.nd.from_dlpack(torch.utils.dlpack.to_dlpack(arg)),
+                    )
+            self.rt_mod.run()
+            return [RelayRTModule._to_torch_tensor(self.rt_mod.get_output(i)) for i in range(self.rt_mod.get_num_outputs())]
+            
 def build_rt_mod(func, example_inputs, tuning_config):
     jit_mod = torch.jit.trace(func, example_inputs)
     shape_list = [(f"inp_{idx}", i.shape) for idx, i in enumerate(example_inputs)]
@@ -33,18 +52,5 @@ def build_rt_mod(func, example_inputs, tuning_config):
     dev = tvm.cpu(0)
     mod_after_tuning = tuning_relay(mod, params, tuning_config)
     rt_mod = graph_executor.GraphModule(mod_after_tuning["default"](dev))
-    
-    to_torch_tensor = lambda nd_tensor : torch.utils.dlpack.from_dlpack(nd_tensor.to_dlpack())
-    
-    def exec_tvm(*args):
-        args = [a.contiguous() for a in args]
-        for idx, arg in enumerate(args, 0):
-            if arg.dim() != 0:
-                rt_mod.set_input(
-                    f"inp_{idx}",
-                    tvm.nd.from_dlpack(torch.utils.dlpack.to_dlpack(arg)),
-                )
-        rt_mod.run()
-        return [to_torch_tensor(rt_mod.get_output(i)) for i in range(rt_mod.get_num_outputs())]
 
-    return exec_tvm
+    return RelayRTModule(rt_mod)
